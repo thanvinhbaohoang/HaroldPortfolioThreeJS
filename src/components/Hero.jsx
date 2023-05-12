c0db7b36f66568af215663a788cd66d2bf33ffc1
Harold <harold@pop-os.localdomain>
2023-05-12 16:43:08 -0400

    Added ComputerCanvas back

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
 
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
    
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
