5b98b4e55d1d68e3d0f923fa418486ffbe7cf787
Harold <harold@pop-os.localdomain>
2023-05-14 15:55:12 -0400

    Changed Hero Button z-index

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             </p>
           </div>
           
-          <div className="mt-8">
+          <div className="mt-8 z-10 ">
             <button class="relative inline-flex items-center justify-center p-0.5 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <a href= "src/constants/Harold-College-CS-Resume.pdf" title="" download class="relative px-5 py-2.5 transition ease duration-75 hover:scale-105 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   My Resume
